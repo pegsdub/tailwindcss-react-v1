41500fd9ef62400823a3b3d40a45235752deb511
Nate Pegram <pegsdub@gmail.com>
2022-06-11 16:59:40 -0700

    Fix tailwind.config.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,17 +7,13 @@ module.exports = {
     extend: {
       animation: {
         wiggle: 'wiggle 1s ease-in-out infinite',
-      },
-      animation: {
         text: 'text 5s ease infinite',
       },
       keyframes: {
         wiggle: {
           '0%, 100%': { transform: 'rotate(-3deg)' },
           '50%': { transform: 'rotate(3deg)' },
-        }
-      },
-      keyframes: {
+        },
         text: {
           '0%, 100%': {
             'background-size': '200% 200%',
