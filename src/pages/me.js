99b5663200d11edf3e8a93b2b3e285b18c8154ec
Nate Pegram <pegsdub@gmail.com>
2022-06-11 23:01:21 -0700

    Aesthetic changes or however you spell it lol.

--- src/pages/me.js
+++ src/pages/me.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const Me = () => {
     return (
-        <div className='snap-y snap-mandatory h-screen w-screen overflow-scroll'>
+        <div className='h-screen w-screen overflow-scroll'>
             <div className='snap-start w-screen h-screen flex items-center justify-center'>
                 <h1 className="text-center lg:text-7xl md:text-7xl sm:text-5xl text-3xl font-extralight mb-14 underline decoration-green-200">
                     i am a lover of all things green...
@@ -20,7 +20,7 @@ const Me = () => {
                 </h1>
             </div>
             <div className='snap-start bg-white w-screen h-screen flex items-center justify-center'>
-                <h1 className="text-center lg:text-7xl md:text-7xl sm:text-5xl text-3xl font-extralight mb-14 underline decoration-black">
+                <h1 className="p-8 m-12 rounded-full bg-green-100 text-center shadow-lg lg:text-7xl md:text-7xl sm:text-5xl text-3xl font-extralight mb-14 animate-bounce">
                     <Link to='/creations'> check out some of the projects that i have created</Link>
                 </h1>
             </div>
