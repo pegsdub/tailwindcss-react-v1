49383179c1961087f0bf9cc4f49f0528831bc171
Nate Pegram <pegsdub@gmail.com>
2022-06-17 21:23:40 -0700

    Changed to notepad. Will make notion project.

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -19,9 +19,8 @@ const Hero = () => {
                 </div>
             </div>
             <Link to='/bouncy' className="-m-2 py-3 px-5 bg-transparent font-extralight rounded-full lg:text-2xl md:text-lg base:text-sm hover:bg-green-100 transition duration-1000 ease-in-out flex flex-row items-center animate-bounce shadow-xl">
-                <em>snek game</em>
-                <svg class="w-6 h-6 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-            </Link>
+                <em>need a notepad?</em>
+                <svg class="w-6 h-6 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"></path></svg>            </Link>
         </div>
     );
 };
